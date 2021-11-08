cae02878ffa9c541b6b0d822e23671a39482877f
StephhyL <Stephanie Li>
2021-11-08 14:13:19 -0500

    added logic to the new route involving route parameter

--- express_server.js
+++ express_server.js
@@ -9,11 +9,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
-  res.render("urls_index", templateVars)
-})
-
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,6 +21,17 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars)
+})
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  res.render("urls_show", templateVars)
+})
+
 
 // app.get("/set", (req, res) => {
 //   const a = 1; 
