fbaf636fe0195f33f5cc0f29cef3290fc44720b0
StephhyL <Stephanie Li>
2021-11-09 12:37:24 -0500

    removed unnecessary post

--- express_server.js
+++ express_server.js
@@ -43,7 +43,6 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars)
 })
 
-
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
@@ -71,12 +70,6 @@ app.post("/urls/:id", (req, res)=>{
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL]
   res.redirect("/urls");
-})
-
-//click on edit button, it will bring redirect server to views urls/:shortURL
-app.post("/urls/:shortURL/edit", (req, res) => {
-  const shortURL = req.params.shortURL;
-  res.redirect(`/urls/${shortURL}`);
 })
 
 
