3bcb7cb4ee527d9ed98e71b930a10c47086a15cd
StephhyL <Stephanie Li>
2021-11-11 13:25:16 -0500

    moved urlsForUsers and generateRandomString into helpers.js file

--- helpers.js
+++ helpers.js
@@ -7,4 +7,19 @@ const getUserByEmail = (email, database) => {
   return false;
 };
 
-module.exports = {getUserByEmail};
+const urlsForUser = (id, database) => {
+  let personalURLs = {};
+  for (let shortURL in database) {
+    if (id === database[shortURL].userID) {
+      personalURLs[shortURL] = database[shortURL]
+    }
+  }
+  return personalURLs;
+} 
+
+const generateRandomString = () => {
+  let randomString = (Math.random() + 1).toString(36).substring(2, 8);
+  return randomString;
+};
+
+module.exports = {getUserByEmail, urlsForUser, generateRandomString};
