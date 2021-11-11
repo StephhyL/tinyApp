36f96d971d84406975e51c6707ac455d755b0d6f
StephhyL <Stephanie Li>
2021-11-11 13:59:18 -0500

    linted

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,26 +4,26 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', () => {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
     assert.strictEqual(user, expectedUserID);
   });
 
   it('should return undefined with an invalid email', () => {
-    const user = getUserByEmail("hello@there.com", testUsers)
+    const user = getUserByEmail("hello@there.com", testUsers);
     const expectedUserID = undefined;
     assert.strictEqual(user, expectedUserID);
   });
