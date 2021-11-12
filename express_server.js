da7f467d655da0855a4b025a9445f4ebc9aa746b
StephhyL <Stephanie Li>
2021-11-11 21:22:57 -0500

    added more comments

--- express_server.js
+++ express_server.js
@@ -25,7 +25,6 @@ app.get("/", (req, res) => {
   if (userID) {
     return res.redirect("/urls");
   } 
-
   return res.redirect("/login");
 });
 
@@ -35,27 +34,25 @@ app.get("/urls", (req, res) => {
   if (userID) {
     const templateVars = {users, user_id: userID, urls: urlsForUser(userID, urlDatabase)};
     return res.render("urls_index", templateVars);
-  } else {
-    // If not logged in, set error code and render error page.
-    const messageOb = {message: "Please login to view URLs", user_id: req.session.user_id, users};
-    res.status(404);
-    return res.render("error_login.ejs", messageOb);
   }
+  // If not logged in, set error status and render error page.
+  const messageOb = {message: "Please login to view URLs", user_id: req.session.user_id, users};
+  res.status(404);
+  return res.render("error_login.ejs", messageOb);
 });
 
 app.get("/urls/new", (req,res) => {
   const userID = req.session.user_id;
   const templateVars = {users, user_id: userID};
 
-  // Checking if user logged in. If logged in, render to create new link page.
+  // Checking if user logged in. If logged in, render to 'create new link' page.
   if (userID) {
     return res.render("urls_new", templateVars);
-  } else {
-    // If not logged in, set error code and render error page.
-    const messageOb = {users, message: "Please login to create new URLs", user_id: userID};
-    res.status(404);
-    return res.render("error_login.ejs", messageOb);
   }
+  // If not logged in, set error code and render error page.
+  const messageOb = {users, message: "Please login to create new URLs", user_id: userID};
+  res.status(404);
+  return res.render("error_login.ejs", messageOb);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -63,7 +60,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const personalDb = urlsForUser(userID, urlDatabase);
   const shortURL = req.params.shortURL;
 
-  // Checking if user logged in or if the URL belongs to the specific user
+  // Checking if user logged in or if the URL belongs to the specific user.
   if (!userID) {
     const loginMessage = {users, message: "Please login to view your URLs", user_id: userID};
     return res.render("error_login.ejs", loginMessage);
@@ -71,7 +68,7 @@ app.get("/urls/:shortURL", (req, res) => {
     const messageOb = {users, message: "Sorry, you do not have this URL", user_id: userID};
     return res.render("error_url.ejs", messageOb);
   } else {
-    // All good. Render to urls_show
+    // All good. Render to specific shortURL page.
     const longURL = personalDb[req.params.shortURL].longURL;
     const templateVars = {shortURL, longURL, users, user_id: userID};
     return res.render("urls_show", templateVars);
@@ -79,39 +76,38 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  
+  // Checks if URL for given id exists. If so, redirect to longURL page.
   if (urlDatabase[req.params.shortURL] !== undefined) {
     const longURL = urlDatabase[req.params.shortURL].longURL;
     return res.redirect(longURL);
-  } else {
-    const messageOb = {message: "Sorry, URL does not exist", user_id: req.session.user_id, users};
-    res.status(404);
-    return res.render("error_url.ejs", messageOb);
-  }
+  } 
+
+  // If URL for given id does not exists, set error code and render to error page.
+  const messageOb = {message: "Sorry, URL does not exist", user_id: req.session.user_id, users};
+  res.status(404);
+  return res.render("error_url.ejs", messageOb);
 });
 
 app.get("/register", (req,res) => {
   const userID = req.session.user_id;
   const templateVars = {users, user_id: userID};
   
-  // checks if a user is logged in. If so, redirect to URL page. If not, display registration page
+  // Checks if a user is logged in. If so, redirect to URL page. If not, display registration page
   if (userID) {
     return res.redirect('/urls');
-  } else {
-    return res.render("register_new", templateVars);
-  }
+  } 
+  return res.render("register_new", templateVars);
 });
 
 app.get("/login", (req, res) => {
   const userID = req.session.user_id;
   const templateVars = {users, user_id: userID};
 
-  // checks if a user is logged in. If so, redirect to URL page. If not, display login page
+  // Checks if a user is logged in. If so, redirect to URL page. If not, display login page
   if (userID) {
     return res.redirect('/urls');
-  } else {
-    return res.render("login", templateVars);
   }
+  return res.render("login", templateVars);
 });
 
 app.get("/*", (req, res) => {
@@ -127,14 +123,13 @@ app.post("/urls", (req, res) => {
   const longURL = req.body.longURL;
   const userID = req.session.user_id;
   
-  // checks if logged in
+  // Checks if user is logged in. If so, redirect to specific shortURL page.
   if (userID) {
     urlDatabase[shortURL] = {longURL, userID};
     return res.redirect(`urls/${shortURL}`);
-  } else {
-    // Sends error if user is not logged in
-    return res.status(418).send("Only Teapots can brew tea!");
   }
+  // If not logged in, set error code and send error message.
+  return res.status(418).send("Only Teapots can brew tea!");
 });
 
 app.post("/urls/:id", (req, res)=>{
@@ -143,13 +138,13 @@ app.post("/urls/:id", (req, res)=>{
   const editURLUserId = urlDatabase[req.params.id].userID;
   const userID = req.session.user_id;
 
-  // Checks if the user trying to change the URL is the owner of the URL
+  // Checks if the user trying to change the URL is the owner of the URL. If so, update longURL, redirect to URL page.
   if (userID === editURLUserId) {
     urlDatabase[shortURL].longURL = newlongURL;
     return res.redirect('/urls');
-  } else {
-    return res.status(418).send("Teapots can't brew coffee!");
   }
+  // If not logged in, set error code and send error message.
+  return res.status(418).send("Teapots can't brew coffee!");
 });
 
 
@@ -158,7 +153,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   const userID = req.session.user_id;
   const deleteURLUserId = urlDatabase[req.params.shortURL].userID;
 
-  // Checking if the user trying to change the URL is the owner of the URL. If so, delete, and redirect to /urls
+  // Checking if the user trying to change the URL is the owner of the URL. If so, delete, and redirect to URL page.
   if (userID === deleteURLUserId) {
     delete urlDatabase[req.params.shortURL];
     return res.redirect("/urls");
@@ -176,7 +171,7 @@ app.post("/login", (req, res) => {
   const testPassword = req.body.password;
   const user = getUserByEmail(testEmail, users);
   
-  //Checking if the email or password are empty or if an email is not in our database
+  // Checking if the email or password are empty or if an email is not in our database.
   if (!testEmail || !testPassword) {
     return res.status(404).send("Error! Please enter an email address and/or password!");
   } else if (!users[user]) {
@@ -184,7 +179,7 @@ app.post("/login", (req, res) => {
   }
   
   const comparePass = bcrypt.compareSync(testPassword, users[user].password);
-  // compares if the hashed password of the inputted password matches the one in our database
+  // Compares if the hashed password of the inputted password matches the one in our database
   if (comparePass) {
     // login successful: set cookie session.
     req.session.user_id = users[user].id;
@@ -205,16 +200,16 @@ app.post("/register", (req, res) => {
   const id = generateRandomString();
   const email = req.body.email;
   const password = req.body.password;
-  //Hashing the password prior to saving to the database
+  // Hashing the password prior to saving to the database
   const hashedPassword = bcrypt.hashSync(password, 10);
 
-  //Checking if the email or password are empty or if an email already exists
+  // Checking if the email or password are empty or if an email already exists
   if (!email|| !password) {
     return res.status(401).send(`Error! Please input an email and/or password.`);
   } else if (getUserByEmail(email, users)) {
     return res.status(401).send(`Error! E-mail already exists.`);
   } else {
-    //Resigration successful: create a new User, save to database, create cookie session, redirect /urls
+    // Resigration successful: create a new User, save to database, create cookie session, redirect /urls
     const user = {id, email, password: hashedPassword};
     users[id] = user;
     req.session.user_id = id;
